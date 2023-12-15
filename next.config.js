db0e5cb0107c619badd1c5dd0148d8940981b147
Pegu-Mrigen <mrig8520@gmail.com>
2023-12-15 15:43:13 +0530

    2n15122023

--- next.config.js
+++ next.config.js
@@ -1,25 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    images:{
-        remotePatterns:[
-            
-            {protocol:"https",hostname:"*.googleusercontent.com"},
-            {protocol:"https",hostname:"firebasestorage.googleapis.com"},
-        ]
-    },
-    
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false,
-        path: false };
-  
-      return config;
-    },
-    
+  images:{
+      remotePatterns:[
+          
+          {protocol:"https",hostname:"*.googleusercontent.com"},
+          {protocol:"https",hostname:"firebasestorage.googleapis.com"},
+      ]
+  },
+  // webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 }
 
-
-
-
-
-
+module.exports = nextConfig
